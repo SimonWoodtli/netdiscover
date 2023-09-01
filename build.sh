56514b776d8eb91234b1026fa33fbd8da1abe91d
SimonWoodtli <xnasero@posteo.net>
2023-09-01 12:14:46 -0500

    🔨 add build.sh script

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -uex pipefail
+
+apt-get update -y && apt-get install -y libpcap-dev libnet1-dev build-essential bison
+git clone https://github.com/netdiscover-scanner/netdiscover.git /tmp/netdiscover
+cd /tmp/netdiscover
+
+sh update-oui-database.sh && ./autogen.sh && ./configure && make && make install
